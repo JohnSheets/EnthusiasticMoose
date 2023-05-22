2c08dad19adef54c6825b77d9766ea0c5e2915ec
John Sheets <johnsheets410@gmail.com>
2023-05-22 14:15:56 -0400

    Phase 6

--- Program.cs
+++ Program.cs
@@ -5,8 +5,12 @@ Console.WriteLine("--------------------------------------------");
 Console.WriteLine();
 
 // Let the moose speak!
-MooseSays("H I, I'M  E N T H U S I A S T I C !");
+// MooseSays("H I, I'M  E N T H U S I A S T I C !");
 MooseSays("I really am enthusiastic");
+
+// As a question
+bool isTrue = MooseAsks("Is Canada real?");
+Console.WriteLine(isTrue);
 
 void MooseSays(string message)
 {
@@ -38,4 +42,25 @@ void MooseSays(string message)
                       \ \'._  ` '_.'
                        `^^` `^^^`
     ");
+}
+
+bool MooseAsks(string question)
+{
+    Console.Write($"{question} (Y/N): ");
+    string answer = Console.ReadLine().ToLower();
+
+    while (answer != "y" && answer != "n")
+    {
+        Console.Write($"{question} (Y/N): ");
+        answer = Console.ReadLine().ToLower();
+    }
+
+    if (answer == "y")
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
